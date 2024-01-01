21da6d0f21af15b72c6facf2ba38cdea6588a1ec
Igor <30476065+Igor-san@users.noreply.github.com>
2024-01-01 17:59:19 +0300

    11.3 Decompose. Первые компоненты

---
+++ app/src/main/java/com/example/mvidecomposetest/presentation/EditContactComponent.kt
@@ -0,0 +1,21 @@
+package com.example.mvidecomposetest.presentation
+
+
+import kotlinx.coroutines.flow.StateFlow
+
+interface EditContactComponent {
+
+    val model: StateFlow<Model>
+
+    fun onUsernameChanged(username: String)
+
+    fun onPhoneChanged(phone: String)
+
+    fun onSaveContactClicked()
+
+    data class Model(
+
+        val username: String,
+        val phone: String
+    )
+}
